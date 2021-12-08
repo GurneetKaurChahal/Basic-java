c0840a6b0118b363f268a47e06ca9b402d5dfd24
GurneetKaurChahal <95748613+GurneetKaurChahal@users.noreply.github.com>
2021-12-08 15:56:27 +0530

    Add files via upload

---
+++ copy.java
@@ -0,0 +1,27 @@
+/**
+ * copy
+ */
+public class copy {
+  public static void main(String[] args) {
+    /*int[] arr1 = new int[]{34,545,67};
+    int[] arr2 =new int[arr1.length]; 
+    
+  int i ;  
+
+    for( i=0; i<arr1.length ; i++){
+      arr2[ i] = arr1[ i];
+        System.out.println(arr2[i]);
+    }
+  }*/
+   int[][] check = {{21,86},{97,37}};
+   for(int i=0; i<check.length ; i++)
+   for( int j=0; j<check.length ;j++ )
+   System.out.println(check[i][i]);
+   
+  
+
+
+
+
+}
+}
